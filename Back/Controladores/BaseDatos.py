9488da3f4270ccdcb9ae1a7646407027a7eaa91e
JDGA1997 <105946879+JDGA1997@users.noreply.github.com>
2022-10-17 02:02:38 -0300

    Back

---
+++ Back/Controladores/BaseDatos.py
@@ -0,0 +1,11 @@
+#Generar la Base de Datos
+
+#Cargar usuario
+#Consultar usuario
+#Update usuario
+#Delete usuario
+
+#Cargar producto
+#Update producto
+#Delete producto
+#Consultar producto
---
+++ Back/Controladores/ProductoController.py
@@ -0,0 +1,9 @@
+# Index()
+# ComprarProducto(id) -> redirige a la página del vendedor
+# GetProducto(id)
+# GetAllProductos()
+
+## Solo ADMIN
+# AgregarProducto()
+# EliminarProducto(id)
+# ModificarProducto(id)
